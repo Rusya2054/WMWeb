93fe2aaafeb49279c1f6fb824f84f4e01f5f1c04
aubakirov <Aubakirov@togi.ru>
2024-11-26 15:24:29 +0500

    -added filtration by frequently occurring values

--- src/main/java/com/Rusya2054/wm/web/files/calculations/CalculationLiquidFlowRatioSmoothed.java
+++ src/main/java/com/Rusya2054/wm/web/files/calculations/CalculationLiquidFlowRatioSmoothed.java
@@ -2,41 +2,62 @@ package com.Rusya2054.wm.web.files.calculations;
 
 import com.Rusya2054.wm.web.models.Indicator;
 
+import java.security.KeyStore;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Function;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class CalculationLiquidFlowRatioSmoothed {
     public static Map<LocalDateTime, Float> getLiquidFlowRatioSmoothed(List<Indicator> indicators){
-//        final Map<LocalDateTime, Float> result = new TreeMap<>(LocalDateTime::compareTo);
-//
-//        final Map<LocalDateTime, Float> subResult = IntStream.range(0, indicators.size())
-//                .boxed()
-//                .filter(index-> index%inputLazyPlotParameter == 0)
-//                .collect(Collectors.toMap(
-//                        index-> indicators.get(index).getDateTime(),
-//                        index -> indicators.get(index).getLiquidFlowRatio(),
-//                        (existing, replacement) -> existing
-//                ));
-//        result.putAll(subResult);
         Set<LocalDate> uniqueDays = indicators.stream().map(i->i.getDateTime().toLocalDate()).collect(Collectors.toSet());
+        Map<LocalDateTime, List<Float>> uniqueDaysListMap = new HashMap<>(uniqueDays.size());
         Map<LocalDateTime, Float> averagedDateMap = new TreeMap<>(LocalDateTime::compareTo);
         uniqueDays.forEach(day->{
-             double average = indicators.stream()
+             List<Float> values = indicators.stream()
                      .filter(i -> i.getLiquidFlowRatio() > 0)
-                     .filter(i -> i.getDateTime().toLocalDate().equals(day))
-                     .mapToDouble(Indicator::getLiquidFlowRatio)
-                     .average()
-                     .orElse(0.0);
+                     .filter(i -> i.getDateTime().toLocalDate().equals(day)).map(Indicator::getLiquidFlowRatio).toList();
 
-            averagedDateMap.put(day.atStartOfDay(), (float)average);
+            uniqueDaysListMap.put(day.atStartOfDay(), values);
         });
+        uniqueDaysListMap.entrySet().forEach(e->{
+            Map<Float, Integer> frequencyMap = e.getValue().stream()
+                    .collect(Collectors.toMap(
+                            Function.identity(),
+                            v->1,
+                            Integer::sum));
 
+            Map<Integer, Float> invertedFrequencyMap = frequencyMap.entrySet().stream()
+                    .collect(Collectors.toMap(
+                            Map.Entry::getValue,
+                            Map.Entry::getKey,
+                            (a, b) -> a
+                    ));
+
+            if (frequencyMap.size() > 2){
+                List<Integer> maxValues = frequencyMap.values().stream()
+                        .sorted(Comparator.reverseOrder())
+                        .limit(2)
+                        .collect(Collectors.toList());
+                if (((float)maxValues.get(0)/(float)maxValues.get(1)) >= 10.0f){
+                    Float valueToFilter = invertedFrequencyMap.get(maxValues.get(0));
+                    Float averagedValue = (float) e.getValue().stream().filter(v->!v.equals(valueToFilter))
+                    .mapToDouble(Float::doubleValue).average().orElse(0.0);
+                    averagedDateMap.put(e.getKey(), averagedValue);
+                } else {
+                    Float averagedValue = (float) e.getValue().stream()
+                    .mapToDouble(Float::doubleValue).average().orElse(0.0);
+                    averagedDateMap.put(e.getKey(), averagedValue);
+                }
+            }
+
+        });
         return averagedDateMap;
     }
 }
